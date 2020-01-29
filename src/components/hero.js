f8d781d279a709a9bd1f9da6276daec00093c774
Javier Zapata <zapataj87@gmail.com>
2020-01-28 18:48:29 -0600

    fixed styling issue

--- src/components/hero.js
+++ src/components/hero.js
@@ -32,11 +32,12 @@ const Header = ({ siteTitle }) => (
       to get your tickets and show it when you are making your purchase!
     </p>
 
-
+    <div style={{ margin: 60, width: `250px`, position: "relative" }}>
 
         <MockupContent />
 
 
+    </div>
     <button id="example-widget-trigger" type="button">Buy Tickets</button>
   </div>
 )
